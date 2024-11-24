55fd6d2de5e339c884d33993b548d76ed20a621c
decimaldreamer <35432275+decimaldreamer@users.noreply.github.com>
2024-11-24 20:31:31 +0300

    Checking status of service

--- uintptrDPI/Form1.Designer.cs
+++ uintptrDPI/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace uintptrDPI
+namespace uintptrDPI
 {
     partial class Form1
     {
@@ -32,22 +32,24 @@
             listBoxLogs = new ListBox();
             btnInstallService = new Button();
             Close = new Button();
+            btnCheckStatus = new Button();
             SuspendLayout();
             // 
             // listBoxLogs
             // 
             listBoxLogs.FormattingEnabled = true;
             listBoxLogs.ItemHeight = 15;
+            listBoxLogs.Items.AddRange(new object[] { "Waiting input..." });
             listBoxLogs.Location = new Point(7, 8);
             listBoxLogs.Name = "listBoxLogs";
-            listBoxLogs.Size = new Size(704, 139);
+            listBoxLogs.Size = new Size(861, 184);
             listBoxLogs.TabIndex = 0;
             // 
             // btnInstallService
             // 
-            btnInstallService.Location = new Point(12, 153);
+            btnInstallService.Location = new Point(7, 198);
             btnInstallService.Name = "btnInstallService";
-            btnInstallService.Size = new Size(354, 40);
+            btnInstallService.Size = new Size(375, 40);
             btnInstallService.TabIndex = 1;
             btnInstallService.Text = "Install Service";
             btnInstallService.UseVisualStyleBackColor = true;
@@ -55,19 +57,30 @@
             // 
             // Close
             // 
-            Close.Location = new Point(372, 153);
+            Close.Location = new Point(671, 198);
             Close.Name = "Close";
-            Close.Size = new Size(339, 40);
+            Close.Size = new Size(197, 40);
             Close.TabIndex = 2;
             Close.Text = "Close";
             Close.UseVisualStyleBackColor = true;
             Close.Click += Close_Click;
             // 
+            // btnCheckStatus
+            // 
+            btnCheckStatus.Location = new Point(388, 197);
+            btnCheckStatus.Name = "btnCheckStatus";
+            btnCheckStatus.Size = new Size(277, 41);
+            btnCheckStatus.TabIndex = 3;
+            btnCheckStatus.Text = "Check Status";
+            btnCheckStatus.UseVisualStyleBackColor = true;
+            btnCheckStatus.Click += btnCheckStatus_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(723, 206);
+            ClientSize = new Size(872, 246);
+            Controls.Add(btnCheckStatus);
             Controls.Add(Close);
             Controls.Add(btnInstallService);
             Controls.Add(listBoxLogs);
@@ -86,5 +99,6 @@
         private ListBox listBoxLogs;
         private Button btnInstallService;
         private Button Close;
+        private Button btnCheckStatus;
     }
 }
